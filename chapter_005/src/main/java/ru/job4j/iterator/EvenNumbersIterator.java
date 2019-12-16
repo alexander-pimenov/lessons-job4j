ebdc61f2ff7c114f3afe1e13704ac39bc2553298
Alexander Pimenov <pimalex1978@yandex.ru>
2019-12-16 14:54:30 +0300

    Изменил метод next() для EvenNumbersIterator, убрал цикл while() из него.

--- chapter_005/src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ chapter_005/src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -3,6 +3,11 @@ package ru.job4j.iterator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * Создаем итератор возвращающий только четные цифры.
+ * Итератор может принимать список произвольных чисел.
+ */
+
 public class EvenNumbersIterator implements Iterator {
 
     private final int[] values;
@@ -25,6 +30,20 @@ public class EvenNumbersIterator implements Iterator {
         return result;
     }
 
+    /*  //Можно использовать и этот метод для next(),
+        //чтобы избежать цикла while() в методе next(),
+        //Можем раскомментировать и всё будет исправно работать.
+        @Override
+        public Object next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            var result = values[index];
+            index++;
+            return result;
+        }
+    */
     @Override
     public Object next() {
         if (!hasNext()) {
