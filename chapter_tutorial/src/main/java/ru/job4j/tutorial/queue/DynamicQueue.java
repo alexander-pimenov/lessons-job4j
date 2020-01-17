2c6105c4f4763356b2714ce133be9f18b85fcca7
Alexander Pimenov <pimalex1978@yandex.ru>
2020-01-17 23:13:33 +0300

    Реализация интерфейса Queue, на примерах связной очереди, динамической очереди

---
+++ chapter_tutorial/src/main/java/ru/job4j/tutorial/queue/DynamicQueue.java
@@ -0,0 +1,123 @@
+package ru.job4j.tutorial.queue;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class DynamicQueue<E> implements Queue<E> {
+    private final int INITIAL_ARRAY_LENGTH = 3;
+    private int size = 0;
+    private E[] array;
+
+    public DynamicQueue() {
+        this.array = (E[]) new Object[INITIAL_ARRAY_LENGTH];
+    }
+
+    @Override
+    public void offer(E e) {
+        if (size() != array.length)
+            array[size] = e;
+        else {
+            array = Arrays.copyOf(array, array.length * 2);
+            array[this.size] = e;
+        }
+        size++;
+    }
+
+    @Override
+    public E remove() {
+        if (this.size == 0)
+            throw new NoSuchElementException();
+        E element = array[0];
+
+        for (int i = 0; i < this.size; i++) {
+            if (i == array.length)
+                array = Arrays.copyOf(array, array.length * 2);
+            array[i] = array[i + 1];
+        }
+
+        size--;
+        return element;
+    }
+
+    @Override
+    public E poll() {//Берем 0 элемент и сдвигаем все остальные элементы на 1 влево.
+        E element = array[0];
+
+        for (int i = 0; i < this.size; i++) {
+            if (i == array.length)
+                array = Arrays.copyOf(array, INITIAL_ARRAY_LENGTH * 2);
+            array[i] = array[i + 1];
+        }
+
+        size--;
+        return element;
+    }
+
+    @Override
+    public E element() {
+        if (size == 0)
+            throw new NoSuchElementException();
+        return array[0];
+    }
+
+    @Override
+    public E peek() {
+        return array[0];
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    @Override
+    public boolean contains(E elem) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(elem))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new QueueIterator();
+    }
+
+    private class QueueIterator implements Iterator {
+
+        private int currentElement;
+
+        public QueueIterator() {
+            this.currentElement = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return currentElement != size;
+        }
+
+        @Override
+        public Object next() {
+            return array[currentElement++];
+        }
+    }
+
+    public static void main(String[] args) {
+        DynamicQueue<Integer> integers = new DynamicQueue<>();
+        integers.offer(1);
+        integers.offer(2);
+        integers.offer(3);
+        integers.offer(4);
+
+        for (Integer a : integers) {
+            System.out.println(a);
+        }
+    }
+}
---
+++ chapter_tutorial/src/main/java/ru/job4j/tutorial/queue/LinkedListQueue.java
@@ -0,0 +1,70 @@
+package ru.job4j.tutorial.queue;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedListQueue<E> implements Queue<E> {
+    private LinkedList<E> list;
+
+    public LinkedListQueue() {
+        this.list = new LinkedList<>();
+    }
+
+    @Override
+    public E peek() {
+        return list.peek();
+    }
+
+    @Override
+    public E element() {
+        return list.element();
+    }
+
+    @Override
+    public E poll() {
+        return list.poll();
+    }
+
+    @Override
+    public E remove() {
+        return list.remove();
+    }
+
+
+    @Override
+    public void offer(E e) {
+        list.offer(e);
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return list.iterator();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public boolean contains(Object o) {
+        return list.contains(o);
+    }
+
+    public static void main(String[] args) {
+        LinkedListQueue<Integer> integers = new LinkedListQueue<>();
+
+        integers.offer(1);
+        integers.offer(2);
+        integers.offer(4);
+        integers.offer(5);
+
+        for (Integer a : integers) {
+            System.out.println(a);
+        }
+    }
+}
