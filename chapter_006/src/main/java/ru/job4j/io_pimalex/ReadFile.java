8a666d60ccb6cee96830026b04e732b0f9face42
Alexander Pimenov <pimalex1978@yandex.ru>
2020-06-20 21:43:47 +0300

    Читаем файл [4898#210810]

---
+++ chapter_006/src/main/java/ru/job4j/io_pimalex/ReadFile.java
@@ -0,0 +1,27 @@
+package ru.job4j.io_pimalex;
+
+import java.io.FileInputStream;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        //try (FileInputStream in = new FileInputStream("input.txt")) {
+        try (FileInputStream in = new FileInputStream(ReadFile.class.getResource("/input.txt").getFile())) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            System.out.println(text);
+
+            System.out.println("============");
+
+            /*Получившийся текст разбиваем на строчки через метод String.split*/
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
